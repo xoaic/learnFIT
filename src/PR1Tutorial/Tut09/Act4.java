8fd300174bb09b17d9b6bfc1c825d97b9cefb4e0
Xoai C <xoaic@xcc.one>
2021-11-19 15:25:42 +0700

    updatePR1.Tut09.Act4

--- src/PR1Tutorial/Tut09/Act4.java
+++ src/PR1Tutorial/Tut09/Act4.java
@@ -1,31 +1,69 @@
 package PR1Tutorial.Tut09;
 
-import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 class Act4 {
+    static int m = 0;
+    static String[] strArray;
+    static int[] intArray;
+
+    // Backtracking algorithm in my mine
+    static String[] createArray(int l, int n) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = l; j < n; j++) {
+                intArray[j] = i;
+                if (j == 4) {
+                    m++;
+                    strArray[m] = Arrays.toString(intArray);
+                    break;
+                }
+                if (i == 1) {
+                    l--;
+                    return strArray;
+                }
+                createArray(j++, n);
+            }
+        }
+        return strArray;
+    }
     public static void main(String[] args) throws IOException {
-        ArrayList<String> S = new ArrayList<String>();
         Scanner input = new Scanner(System.in);
         int n;
         int k;
+        String check1 = "";
+        ArrayList<String> strArray2 = new ArrayList<String>();
         do {
             System.out.print("Enter N and K: ");
             n = input.nextInt();
             k = input.nextInt();
         } while (!(1 <= k) || !(k < n) || !(n <= 32));
-        input.close();
-        for (int i = 0; i < n; i++) {
-            S.add("A");
+        for (int i = 0; i < k; i++) {
+            check1 += "A";
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {}
+        String check2 = check1 + "A";
+        intArray = new int[n];
+        input.close();
+        createArray(0, n);
+        for (int i = 0; i < strArray.length; i++) {
+            strArray[i].replaceAll("0", "A");
+            strArray[i].replaceAll("1", "B");
+            if (strArray[i].contains(check1)) {
+                if (strArray[i].contains(check2)) {
+                    break;
+                }
+                strArray2.add(strArray[i]);
+            }
         }
         try {
-            FileWriter fileOut = new FileWriter("ketqua.txt");
-            
+            PrintWriter fileOut = new PrintWriter("ketqua.txt");
+            fileOut.println(strArray2.size());
+            for (int i = 0; i < strArray2.size(); i++) {
+                fileOut.println(strArray2.get(i));
+            }
             fileOut.close();
         } catch (Exception e) {
             e.getStackTrace();
